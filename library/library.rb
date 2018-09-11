ffa3d839b6ec7815b49baaebae9a0f1ff17befeb
Jordan Davidson <jordandavidson1990@hotmail.com>
2018-09-11 10:53:08 +0100

    class additions

--- library/library.rb
+++ library/library.rb
@@ -22,17 +22,45 @@
 # Create a method that changes the rental details of a book by taking in the title
 # of the book, the student renting it and the date it's due to be returned.
 
-class LibraryArchives
-
-attr_accessor :title, :rental_details, :student_name, :date
+class Library
 
   def initialize(books)
-    @books = books
+    @books = books  || []     # will create an empty array for you if theres no books to assign
   end
 
-  def get_title(title)
+  def get_books
+    return @books
+  end
 
+  def get_book_info(title)
+    found_book = nil        # assigned book to nil at the start
+    for book in @books      # looping for book in @books
+      if book[:title] == title  # if the book title is the same as the title passed through
+        found_book = book     # reassign the found book as book
+      end
+    end
+    return found_book
+  end
 
+  def get_rental_details(title)
+    book = get_book_info(title)     # calling previous function
+    return book[:rental_details]
+  end
+
+  def add_book(title)
+    new_book = {
+      title: title,
+      rental_details: {
+        student_name: "",
+        date: ""
+      }
+    }
+    @books.push(new_book)   # puts it into the @books array
+  end
+
+  def rent_book(title, name, date)
+    book = get_book_info(title)     # gives the book object i want
+    book[:rental_details] = { student_name: name, date: date}   # creating a key value
   end
 
 end
--- library/spec/library_spec.rb
+++ library/spec/library_spec.rb
@@ -5,37 +5,70 @@ require_relative('../library.rb')
 
 class TestLibraryArchives < Minitest::Test
 
-  def setup
-    @library = Library.new(
-      [
-        {
-          title: "lord_of_the_rings",
-          rental_details: {
-            student_name: "Jeff",
-            date: "01/12/18"
-          }
-        },
-        {
-          title: "catcher_in_the_rye",
-          rental_details: {
-            student_name: "Holden",
-            date: "25/12/18"
-          }
-        },
-        {
-          title: "war_and_peace",
-          rental_details: {
-            student_name: "Mary",
-            date: "04/07/18"
-          }
+  def setup()
+    @books_list = [
+      {
+        title: "lord_of_the_rings",
+        rental_details: {
+          student_name: "Jeff",
+          date: "01/12/18"
         }
-      ]
-    )
+      },
+      {
+        title: "catcher_in_the_rye",
+        rental_details: {
+          student_name: "Holden",
+          date: "25/12/18"
+        }
+      },
+      {
+        title: "war_and_peace",
+        rental_details: {
+          student_name: "Mary",
+          date: "04/07/18"
+        }
+      }
+    ]
+    @library = Library.new(@books_list)
   end
-def get_books
-result = @library.title("lord_of_the_rings")
-assert_equal(result, "lord_of_the_rings")
-end
 
+  def test_can_get_books
+    assert_equal(@books_list, @library.get_books())  #calling in the library for the method of get_books
+  end
 
+  def test_can_get_book_info
+    result = @books_list[0]
+    assert_equal(result, @library.get_book_info("lord_of_the_rings"))
+  end
+
+  def test_can_get_rental_details
+    result = {
+      student_name: "Jeff",
+      date: "01/12/18"
+    }
+    assert_equal(result, @library.get_rental_details("lord_of_the_rings"))
+  end
+
+  def test_can_add_book
+    @library.add_book("1984")   # need to write a method adding a title
+    result = {
+      title: "1984",
+      rental_details: {
+        student_name: "",
+        date: ""
+      }
+    }
+    assert_equal(result, @library.get_book_info("1984"))    # calling on previous function to check if its there
+  end
+
+  def test_can_update_rental_details
+    # create the function that allows rent book
+    @library.rent_book("lord_of_the_rings", "Barry", "11/09/18")    # @Library is where you wanna do it
+    renting_info = @library.get_rental_details("lord_of_the_rings") # just need title because previous function on called for title
+    result = {              # create the result variable
+      student_name: "Barry",
+      date: "11/09/18"
+    }
+    assert_equal(result, renting_info)
+  end
 end
